b82d5b8a1fe62ae084348bb02087ee4113e496fd
Rahul Sawant <rahulsawanth06@gmail.com>
2019-06-19 00:54:04 +0530

    Added social media icons in contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,7 +9,43 @@ const Contact = () => {
       <SEO title="Contact" />
       <div className="block-start" id="contact">
         <div className="container">
-          <h1>This is Contact Page</h1>
+          <h2>Feel free to contact me through email,</h2>
+          <a href="mailto:contact@raalzz.com" target="_top">
+            contact@raalzz.com
+          </a>
+          <h3>Or,</h3>
+          <div>
+            <a
+              href="https://twitter.com/Raalzz"
+              target="_blank"
+              rel="nofollow"
+              className="fa fa-twitter"
+            />
+            <a
+              href="https://github.com/Raalzz"
+              target="_blank"
+              rel="nofollow"
+              className="fa fa-github"
+            />
+            <a
+              href="https://www.linkedin.com/in/rahul-sawant-895481109/"
+              target="_blank"
+              rel="nofollow"
+              className="fa fa-linkedin"
+            />
+            <a
+              href="https://www.reddit.com/user/Raalzz619"
+              target="_blank"
+              rel="nofollow"
+              className="fa fa-reddit"
+            />
+            <a
+              href="https://www.last.fm/user/Raalzz"
+              target="_blank"
+              rel="nofollow"
+              className="fa fa-lastfm"
+            />
+          </div>
         </div>
       </div>
     </Layout>
