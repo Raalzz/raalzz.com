c4837b9f25ed3244a45c9f90857486794aaeba24
Rahul Sawant <rahulsawanth06@gmail.com>
2019-06-18 23:28:12 +0530

    Completed about page design

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,18 +11,30 @@ const About = () => {
 
       <div className="block-start" id="about">
         <div className="container">
-          <div>
+          <div className="display_img">
             <img src={display_img} alt="Display" height="150" width="150" />
           </div>
           <h1>Hi, this is Rahul.</h1>
           <p>
-            Currently working in a startup called WheelsBox as a Front-end
-            Developer. I like exploring new Techs and Hopping new Stacks is what
-            I enjoy the most.
+            Currently working in a startup called{" "}
+            <a href="https://wheelsbox.com/" target="_blank" rel="nofollow">
+              WheelsBox
+            </a>{" "}
+            as a Front-end Developer. I like exploring new Techs and Hopping new
+            Stacks is what I enjoy the most.
           </p>
           <p>
-            I enjoy dancing, Dubstep is my forte. I am also a Big EDM and
-            Skrillex Fan. Follow me on last.fm to see my scrobles.
+            My Mediator personality goes by INFP. I enjoy dancing, Dubstep is my
+            forte. I am also a Big EDM and
+            <strong> Skrillex</strong> Fan. Follow me on{" "}
+            <a
+              href="https://www.last.fm/user/Raalzz/"
+              target="_blank"
+              rel="nofollow"
+            >
+              last.fm
+            </a>{" "}
+            to see my scrobles.
           </p>
         </div>
       </div>
