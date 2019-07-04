2c437529d7f4ee27d4dfcc10b2e42d2d4c886917
Rahul Sawant <rahulsawanth06@gmail.com>
2019-07-04 17:43:25 +0530

    Added Google Analytics Tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `YOUR_GOOGLE_ANALYTICS_TRACKING_ID`,
+        trackingId: `UA-143309507-1`,
       },
     },
     {
